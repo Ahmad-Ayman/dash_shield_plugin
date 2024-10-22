6fe02e7d8f97eccb35608994c3d5f10fad1e00ef
Ahmad Ayman <ahmed.ayman1708@gmail.com>
2024-10-22 22:24:41 +0300

    first commit 2

--- android/src/main/kotlin/dev/dash_shield/dash_shield/DashShieldPlugin.kt
+++ android/src/main/kotlin/dev/dash_shield/dash_shield/DashShieldPlugin.kt
@@ -4,38 +4,36 @@ import android.app.Activity
 import android.view.WindowManager
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** DashShieldPlugin */
-class DashShieldPlugin: FlutterPlugin, MethodCallHandler {
+class DashShieldPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   private lateinit var channel : MethodChannel
-  private lateinit var activity: Activity  // To access the current activity's window
+  private var activity: Activity? = null  // Nullable, since activity might not be attached
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "dash_shield")
     channel.setMethodCallHandler(this)
   }
 
-  // Attach the activity to the plugin (to modify the window)
-  override fun onAttachedToActivity(binding: FlutterPlugin.FlutterPluginBinding) {
-    activity = binding.activity
-  }
-
+  // Handle MethodCalls from Flutter
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "preventScreenshots") {
       // Prevent screenshots and screen recording for specific screen
-      activity.window.setFlags(
+      activity?.window?.setFlags(
         WindowManager.LayoutParams.FLAG_SECURE,
         WindowManager.LayoutParams.FLAG_SECURE
       )
       result.success(null)
     } else if (call.method == "preventScreenshotsGlobally") {
       // Prevent screenshots and screen recording globally for the whole app
-      activity.window.setFlags(
+      activity?.window?.setFlags(
         WindowManager.LayoutParams.FLAG_SECURE,
         WindowManager.LayoutParams.FLAG_SECURE
       )
@@ -45,7 +43,24 @@ class DashShieldPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
+  // Attach the activity to the plugin (to modify the window)
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
 
+  override fun onDetachedFromActivity() {
+    activity = null  // Clear reference to avoid leaks
+  }
 
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
 
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null  // Clear reference
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 }
